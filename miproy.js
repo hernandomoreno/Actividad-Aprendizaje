7288898efa20f64d0164936a20423b42daa09f7f
Hernando <hernandomoreno@hotmail.com>
2024-06-27 22:33:19 -0500

    Primer commit que hago

---
+++ miproy.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 = 11;
+let total = 0
+
+total = num1 + num2;
+
+console.log("New cargando github...El total es: "+total)
+
---
+++ miproy3.js
@@ -0,0 +1,11 @@
+
+let base = 56;
+let altura = 20;
+
+let area = 0;
+
+area = base * altura / 2;
+
+console.log("El area es : "+ area);
+
+console.log("Nuevo cambio me encuetnro en mail")
